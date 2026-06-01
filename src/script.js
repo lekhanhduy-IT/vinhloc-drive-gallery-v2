476089da945a3736332716734604d501fb305cfa
lekhanhduy-IT <khanhduyhazo@gmail.com>
2026-06-02 05:46:14 +0700

    split js from html

--- src/script.js
+++ src/script.js
@@ -89,7 +89,6 @@
 // ==============================================================
 // (MÃ CŨ CỦA BẠN BẮT ĐẦU TỪ ĐÂY)
 // const SCRIPT_URL = "https://script.google.com/...";
-
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx3xI-bNWfeffsEH-iIc0yYfF9bHYvAiKZKWIfco6j7Z7GOtOAv7Q8WE1_y9xjen7c/exec";
 const ROOT_FOLDER_ID = "1xWDed1IBzGdCA4r5vbds1x6AF31hSIUT";
 const WM_FOLDER_ID = "1P_YxqI3LzWB4GhM2H7Sk05KrISjIpVc7";
